1849ebcfcadaba0eb455c37865a9f205004ef8a8
z5487111g <z5487111g@gmail.com>
2016-07-22 15:58:51 +0800

    Add files via upload

--- sodagreen_ticket_tracker_python2.py
+++ sodagreen_ticket_tracker_python2.py
@@ -7,7 +7,7 @@ number=1404
 while 1:
   response = urllib.urlopen('https://tixcraft.com/ticket/area/16_soda/'+str(number))
   html = response.read().decode('utf8')
-  print(html)
+  #print(html)
   if number==1404:
     event='8/5同名'
   elif number==1405:
